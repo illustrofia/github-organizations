598560e6328756cfd997345ac4b8b85f628b1030
Luca Airinei <illustrofia@gmail.com>
2022-12-12 16:27:19 +0200

    chore: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient()
 
+export const App = () => {
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   )
 }
 
-export default App
+const Example = () => {
+  const { isLoading, error, data } = useQuery('repoData', () =>
+    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+      res.json()
+    )
+  )
+
+  if (isLoading) {
+    return <>{'Loading...'}</>
+  }
+
+  if (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return <>{'An error has occurred: ' + (error as any).message}</>
+  }
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  )
+}
