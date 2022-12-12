676087957afe74949ddbd04a95a4b4939d3bcf89
Luca Airinei <illustrofia@gmail.com>
2022-12-12 18:16:48 +0200

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useQuery } from 'react-query'
 import './App.css'
-import { Providers } from './components'
-import { getOrganizations } from './lib'
+import { Menu, Providers } from './components'
 
 export const App = () => {
   return (
     <Providers>
-      <Example />
+      <Menu />
     </Providers>
   )
-}
-
-const Example = () => {
-  const {
-    isLoading,
-    error,
-    data: organizations,
-  } = useQuery('organizations', getOrganizations, { initialData: [] })
-
-  if (isLoading) {
-    return <>{'Loading...'}</>
-  }
-
-  if (error) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return <>{'An error has occurred: ' + (error as any).message}</>
-  }
-
-  return <div>{organizations && JSON.stringify(organizations)}</div>
 }
